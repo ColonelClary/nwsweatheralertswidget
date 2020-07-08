42367e25c04bf45dfff8efc2363dc8343c22daee
Dave Miller <github@justdave.net>
2020-07-08 10:39:52 -0400

    Remove unused imports

--- app/src/main/java/net/justdave/nwsweatheralertswidget/MainActivity.java
+++ app/src/main/java/net/justdave/nwsweatheralertswidget/MainActivity.java
@@ -3,16 +3,10 @@ package net.justdave.nwsweatheralertswidget;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.PowerManager;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
